6aa9f30d04ace241f2dbf51b009ae7a8a91998b3
bgreni <42788181+bgreni@users.noreply.github.com>
2023-01-20 04:03:07 +0000

    trying to test for real

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// import * as anchor from "@project-serum/anchor";
-// import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
-// import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js";
-// import { BN } from "bn.js";
+const { clusterApiUrl, Connection, Keypair, PublicKey, LAMPORTS_PER_SOL } = require("@solana/web3.js");
+const anchor = require('@project-serum/anchor');
+// const NodeWallet = require('@project-serum/anchor/dist/cjs/nodewallet');
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { bs58 } = require("@project-serum/anchor/dist/cjs/utils/bytes");
 
 (async () => {
     const pk = core.getInput('wallet-key');
-
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
     const payload = github.context.payload;
     const issueNumber = payload.issue?.number;
     const repoName = payload.repository?.name;
@@ -17,35 +15,30 @@ const github = require('@actions/github');
     
     const regex = /DevMill Bounty: (\d+) SOL/;
     const bountyAmount = Number.parseInt(payload.issue?.body?.match(regex)[1]);
-
-    console.log(JSON.stringify({
-        issueNumber: issueNumber,
-        repoName: repoName,
-        repoOwner: repoOwner,
-        bountyAmount: bountyAmount,
-        pk: pk
-    }));
     
-    core.setOutput('bounty-address', 'placeholder');
-    // const programId = "FAuRwCnsvpMHVBDcL47SGM5XSC7oY5u5u9VU3GDqWaZm";
-    // let connection = new Connection(clusterApiUrl("devnet"));
-    // const wallet = new NodeWallet(Keypair.fromSecretKey(TexEncoder().encode(pk)));
-    // const provider = new anchor.AnchorProvider(connection, wallet, anchor.AnchorProvider.defaultOptions());
+    const programId = "FAuRwCnsvpMHVBDcL47SGM5XSC7oY5u5u9VU3GDqWaZm";
+    let connection = new Connection(clusterApiUrl("devnet"));
+    const wallet = new anchor.Wallet(Keypair.fromSecretKey(bs58.decode(pk)));
+    const provider = new anchor.AnchorProvider(connection, wallet, anchor.AnchorProvider.defaultOptions());
 
-    // const program = await new anchor.Program.at(programId, provider);
-    // await program.methods
-    //     .createBounty(
-    //         issueNumber,
-    //         repoName,
-    //         repoOwner,
-    //         new BN(bountyAmount),
-    //         Math.floor(Date.now() / 1000))
-    //     .accounts({
-    //         bounty: PublicKey.findProgramAddressSync(
-    //             [`bounty${issueNumber}${repoName}`]
-    //         ),
-    //         programId
-    //     }).rpc()
+    const program = await anchor.Program.at(programId, provider);
 
-    // console.log("yay");
+    const [pda, _] = PublicKey.findProgramAddressSync(
+        [`bounty${issueNumber}${repoName}`],
+        program.programId
+    );
+
+    await program.methods
+        .createBounty(
+            issueNumber,
+            repoName,
+            repoOwner,
+            new anchor.BN(bountyAmount * LAMPORTS_PER_SOL),
+            Math.floor(Date.now() / 1000))
+        .accounts({
+            bounty: pda,
+            poster: wallet.publicKey
+        }).rpc()
+
+    core.setOutput('bounty-address', pda.toString());
 })();
